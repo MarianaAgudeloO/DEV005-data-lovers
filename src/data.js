1749ed774cb77bfc2560aff71af73b77eda8ac39
unknown <agudelomariana2503@gmail.com>
2023-02-27 09:35:02 -0500

    Cambios en el filtrado, adición de buscador y ordenado

--- src/data.js
+++ src/data.js
@@ -1,26 +1,147 @@
 import data from './data/rickandmorty/rickandmorty.js';
+
+const characters = data.results;
 //Arreglo de personajes que incluyen en el nombre, el texto ingresado
-function searchCharacter(text) {
+export const searchCharacter = (text) => {
   const results = data.results.filter(character => {
     return character.name.toLowerCase().includes(text.toLowerCase());
   });
   return results;
 }
 //Arreglo de personajes ordenados de A-z
-function orderCharacters(characters) {
+export const orderCharacters = (characters) => {
   characters.sort((a, b) => a.name.localeCompare(b.name));
   return characters;
 }
 
 //Arreglo de personajes ordenados de Z-a
-function orderCharactersDescending(characters) {
+export const orderCharactersDescending = (characters) => {
   characters.sort((a, b) => {
     return b.name.localeCompare(a.name);
   });
   return characters;
 }
-export default {
-  searchCharacter:searchCharacter, 
-  orderCharacters:orderCharacters,
-  orderCharactersDescending:orderCharactersDescending
+
+// Filtrar categorías
+export const filterCategory = (optionSelected, secondSelectElement) => {
+  let arrayCategory;
+  switch (optionSelected) {  
+  case "Especie":{
+    const speciesComplete = characters.map((item) => item.species);
+    const speciesList = speciesComplete.filter((item, index) => speciesComplete.indexOf(item) === index);
+    arrayCategory = speciesList;
+    arrayCategory.forEach((element) => {
+      const templateOptions = `
+       <option value = "${element}">${element}</option>
+       `;
+      secondSelectElement.innerHTML += templateOptions;
+    });
+    secondSelectElement.classList.remove("none");
+    break;
+  }
+  case "Género":{
+    const gendersComplete = characters.map((item) => item.gender);
+    const genderList = gendersComplete.filter((item, index) => gendersComplete.indexOf(item) === index);
+    arrayCategory = genderList;
+    arrayCategory.forEach((element) => {
+      const templateOptions = `
+       <option value = "${element}">${element}</option>
+       `;
+      secondSelectElement.innerHTML += templateOptions;
+    });
+    secondSelectElement.classList.remove("none");
+    break;
+  }
+  case "Origen":{
+    const originComplete = characters.map((item) => item.origin.name);
+    const originsList = originComplete.filter((item, index) => originComplete.indexOf(item) === index);
+    arrayCategory = originsList;
+    arrayCategory.forEach((element) => {
+      const templateOptions = `
+         <option value = "${element}">${element}</option>
+         `;
+      secondSelectElement.innerHTML += templateOptions;
+    });
+    secondSelectElement.classList.remove("none");
+    break;
+  }
+  case "Se encuentra actualmente":{
+    const locationComplete = characters.map((item) => item.location.name);
+    const  locationsList = locationComplete.filter((item, index) => locationComplete.indexOf(item) === index);
+    arrayCategory = locationsList;
+    arrayCategory.forEach((element) => {
+      const templateOptions = `
+           <option value = "${element}">${element}</option>
+           `;
+      secondSelectElement.innerHTML += templateOptions;
+    });
+    secondSelectElement.classList.remove("none");
+    break;
+  }
+  case "Estado de vida":{
+    const  statusComplete = characters.map((item) => item.status);
+    const  statusList = statusComplete.filter((item, index) => statusComplete.indexOf(item) === index);
+    arrayCategory = statusList;
+    arrayCategory.forEach((element) => {
+      const templateOptions = `
+             <option value = "${element}">${element}</option>
+             `;
+      secondSelectElement.innerHTML += templateOptions;
+    });
+    secondSelectElement.classList.remove("none");
+    break;
+  }
+  }
+  return arrayCategory;
 };
+
+// Filtrar personajes por categorías
+export const filterCharacters = (firstSelected, secondSelected, containerCards) =>{
+  const filteredCharacters = characters.filter((character) => {
+    if (firstSelected === "Especie") {
+      return character.species === secondSelected;
+    }
+    if (firstSelected === "Género") {
+      return character.gender === secondSelected;
+    }
+    if (firstSelected === "Origen") {
+      return character.origin.name === secondSelected;
+    }
+    if (firstSelected === "Se encuentra actualmente") {
+      return character.location.name === secondSelected;
+    }
+    if (firstSelected === "Estado de vida") {
+      return character.status === secondSelected;
+    }
+  });
+
+  containerCards.innerHTML = "";
+  filteredCharacters.forEach((character) => {
+    let card = "";
+    card = createCard(character);
+    containerCards.innerHTML += card;
+  });
+  return filteredCharacters;
+}
+
+// Creación de card de cada personaje
+export const createCard = (element) => {
+  const templateCharacterCard = `
+  <div class ="card">
+    <div>
+      <img src = "${element.image}" alt = "${element.name}" class="imgCard"></img>
+    </div>
+    <div>
+      <p>Nombre: ${element.name}</p>
+      <p>Especie: ${element.species}</p>
+      <p>Género: ${element.gender}</p>
+      <p>Estado de vida: ${element.status}</p>
+      <p>Origen: ${element.origin.name}</p>
+      <p>Se encuentra actualmente: ${element.location.name}</p>
+    </div>
+  </div>
+  `;
+  return templateCharacterCard;
+}
+
+
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import data from './data/rickandmorty/rickandmorty.js';
-import datajs from './data.js';
+import { filterCategory, filterCharacters, createCard, searchCharacter, orderCharacters, orderCharactersDescending  } from './data.js';
 
 
 const main = document.getElementById("main");
@@ -22,10 +22,10 @@ const secondSelect = document.getElementById("secondSelect");
 const btnSearch = document.getElementById("search");
 const searchText = document.getElementById("searchText");
 
-//Bucador
+//Buscador
 btnSearch.addEventListener("click", () => {
   const text = searchText.value;
-  const results = datajs.searchCharacter(text)
+  const results = searchCharacter(text)
   main.innerHTML = "";
   results.forEach((character) => {    
     let card = "";
@@ -44,10 +44,10 @@ btnSort.addEventListener("click", () => {
   const sortSelected = sortSelect.value;
   let results = [];
   if (sortSelected === 'A-z'){
-    results = datajs.orderCharacters(characters)
+    results = orderCharacters(characters)
   }
   if (sortSelected === 'Z-a'){
-    results = datajs.orderCharactersDescending(characters)
+    results = orderCharactersDescending(characters)
   }
   
   main.innerHTML = "";
@@ -61,160 +61,21 @@ btnSort.addEventListener("click", () => {
   )
 })
 
-
 // Comportamiento al elegir determinada opción del primer select
 firstSelect.addEventListener("change", () =>{
   const selected = firstSelect.value;
   secondSelect.innerHTML = "";
-
-  if(selected === "Especie"){
-    speciesList.forEach((element) => {
-      const templateOptions = `
-      <option value = "${element}">${element}</option>
-      `;
-      secondSelect.innerHTML += templateOptions;
-    });
-    secondSelect.classList.remove("none");
-    
-  }
-  if(selected === "Género"){
-    gendersList.forEach((element) => {
-      const templateOptions = `
-      <option value = "${element}">${element}</option>
-      `;
-      secondSelect.innerHTML += templateOptions;
-    });
-    secondSelect.classList.remove("none");
-  }  
-  if(selected === "Lugar de origen"){
-    originList.forEach((element) => {
-      const templateOptions = `
-      <option value = "${element}">${element}</option>
-      `;
-      secondSelect.innerHTML += templateOptions;
-    });
-    secondSelect.classList.remove("none");
-  }
-  if(selected === "Location"){
-    locationList.forEach((element) => {
-      const templateOptions = `
-      <option value = "${element}">${element}</option>
-      `;
-      secondSelect.innerHTML += templateOptions;
-    });
-    secondSelect.classList.remove("none");
-  }
-  if(selected === "Estado de vida"){
-    statusList.forEach((element) => {
-      const templateOptions = `
-      <option value = "${element}">${element}</option>
-      `;
-      secondSelect.innerHTML += templateOptions;
-    });
-    secondSelect.classList.remove("none");
-  }  
+  filterCategory(selected, secondSelect); 
+})
+// Comportamiento al elegir determinada opción del segundo select
+secondSelect.addEventListener("change", () => {
+  form.addEventListener("submit", (event) => {
+    event.preventDefault(); // Prevenir que se recargue la página
+    const selected = firstSelect.value;
+    const selectedOption = secondSelect.value;
+    // Filtrar los personajes según la opción seleccionada
+    filterCharacters(selected, selectedOption, main);  
+  });
 })
 
-// Comportamiento del formulario al enviarlo
-form.addEventListener("submit", (event) => {
-  event.preventDefault(); // Prevenir que se recargue la página
-  secondSelect.addEventListener("change", () => {
-    form.addEventListener("submit", (event) => {
-      event.preventDefault(); // Prevenir que se recargue la página
-      const selected = firstSelect.value;
-      const selectedOption = secondSelect.value;
-      // Filtrar los personajes según la opción seleccionada
-      const filteredCharacters = characters.filter((character) => {
-        if (selected === "Especie") {
-          return character.species === selectedOption;
-        }
-        if (selected === "Género") {
-          return character.gender === selectedOption;
-        }
-        if (selected === "Lugar de origen") {
-          return character.origin.name === selectedOption;
-        }
-        if (selected === "Location") {
-          return character.location.name === selectedOption;
-        }
-        if (selected === "Estado de vida") {
-          return character.status === selectedOption;
-        }
-      });
-      // Renderizar los personajes filtrados
-      main.innerHTML = "";
-      let card = "";
-      filteredCharacters.forEach((character) => {        
-        card = createCard(character);
-        main.innerHTML += card;
-      });
-    });
-  })
-  
-  const selected = firstSelect.value;
-  const selectedOption = secondSelect.value;
-  // Filtrar los personajes según la opción seleccionada
-  const filteredCharacters = characters.filter((character) => {
-    if (selected === "Especie") {
-      return character.species === selectedOption;
-    }
-    if (selected === "Género") {
-      return character.gender === selectedOption;
-    }
-    if (selected === "Lugar de origen") {
-      return character.origin.name === selectedOption;
-    }
-    if (selected === "Location") {
-      return character.location.name === selectedOption;
-    }
-    if (selected === "Estado de vida") {
-      return character.status === selectedOption;
-    }
-  });
-
-  // Renderizar los personajes filtrados
-  main.innerHTML = "";
-  filteredCharacters.forEach((character) => {
-    let card = "";
-    card = createCard(character);
-    main.innerHTML += card;
-  });
-});
-
-// funcion para crear cards
-function createCard(element){
-  const templateCharacterCard = `
-  <div class ="card">
-    <div>
-      <img src = "${element.image}" alt = "${element.name}" class="imgCard"></img>
-    </div>
-    <div>
-      <p>Nombre: ${element.name}</p>
-      <p>Especie: ${element.species}</p>
-      <p>Género: ${element.gender}</p>
-      <p>Estado de vida: ${element.status}</p>
-      <p>Origen: ${element.origin.name}</p>
-      <p>Se encuentra actualmente: ${element.location.name}</p>
-    </div>
-  </div>
-  `;
-  return templateCharacterCard;
-}
-
-// filtrado de especie, género, estado de vida, orígen y locación
-
-const species = characters.map(character => character.species);
-const speciesList = species.filter((specie, index) => species.indexOf(specie) === index);
-
-const genders = characters.map(character => character.gender);
-const gendersList = genders.filter((gender, index) => genders.indexOf(gender) === index);
-
-const status = characters.map(character => character.status);
-const statusList = status.filter((stat, index) => status.indexOf(stat) === index);
-
-const locations = characters.map(character => character.location.name);
-const locationList = locations.filter((location, index) => locations.indexOf(location) === index);
-
-const origins = characters.map(character => character.origin.name);
-const originList = origins.filter((origin, index) => locations.indexOf(origin) === index);
 
